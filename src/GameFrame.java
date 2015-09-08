bbae3a3683153d6d8a5a7eaa7deffa99ee2ee787
tdaughton <tessdaughton@gmail.com>
2015-09-08 10:38:12 -0600

    I added game frame to the sourcefile

---
+++ src/GameFrame.java
@@ -0,0 +1,25 @@
+/**
+ * Tess Daughton
+ */
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame
+{
+  private final int SCREEN_WIDTH;
+  private final int SCREEN_HEIGHT;
+
+
+
+
+  public GameFrame()
+  {
+    super();
+    Dimension userScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    this.SCREEN_WIDTH = (int) userScreenSize.getWidth();
+    this.SCREEN_HEIGHT= (int) userScreenSize.getHeight();
+    this.setSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+  }
+
+
+}
