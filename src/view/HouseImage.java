dddbfb426e379bb3910aaf8ddb8a6dab11474bca
Tess Daughton <tessdaughton@gmail.com>
2015-09-10 19:52:34 -0600

    update

--- src/view/HouseImage.java
+++ src/view/HouseImage.java
@@ -18,11 +18,14 @@ public class HouseImage extends JPanel implements KeyListener
   protected static final int GRID_WIDTH = 40;
   private int currentScreenWidth = GameFrame.MAX_SCREEN_WIDTH;
   private int currentScreenHeight = GameFrame.MAX_SCREEN_HEIGHT;
+  protected final int MAX_TILE_AREA = (GameFrame.MAX_SCREEN_WIDTH/GRID_WIDTH) * (GameFrame.MAX_SCREEN_WIDTH/GRID_HEIGHT);
   private TileImage[][] tileImages = new TileImage[GRID_HEIGHT][GRID_WIDTH];
   private OutsideImage outside = new OutsideImage();
   private FloorImage floor = new FloorImage();
   private WallImage wall = new WallImage();
   private TreeImage trees = new TreeImage();
+  private int tHeight = this.getTileHeight();
+  private int tWidth = this.getTileWidth();
   private int zoomFactor = 1;
   private PlayerImage sprite;
 
@@ -48,33 +51,76 @@ public class HouseImage extends JPanel implements KeyListener
     addKeyListener(this);
   }
 
-  private void drawTile(Graphics g)
+  private void drawTile(int xMin, int yMin, int xMax, int yMax, int xAdj, int yAdj,  Graphics g)
   {
-    int tHeight = this.getTileHeight();
-    int tWidth = this.getTileWidth();
-    int x = 0;
-    int y = 0;
-    for (int i = 0; i < GRID_WIDTH; i++)
+    int x = -xAdj;
+    int y = -yAdj;
+    for (int i = xMin; i < xMax; i++)
     {
-      for (int j = 0; j < GRID_HEIGHT; j++)
+      for (int j = yMin; j < yMax; j++)
       {
-        g.drawImage(tileImages[i][j].getTileImage(), x, y, tWidth, tHeight, null);
-        x += tWidth;
+        g.drawImage(tileImages[i][j].getTileImage(), x, y, this.tWidth, this.tHeight, null);
+        x += this.tWidth;
       }
-      y += tHeight;
-      x = 0;
+      y += this.tHeight;
+      x = -xAdj;
     }
   }
 
-  private int getTileHeight()
+  private void setVisibleFrame(Graphics g)
   {
-    return currentScreenHeight / GRID_HEIGHT + zoomFactor;
+    this.tHeight = this.getTileHeight();
+    this.tWidth = this.getTileWidth();
+
+    //what do we want the zoom level to be automatically?
+    //maybe like 10*10 tiles?
+
+    if(tHeight*tWidth>MAX_TILE_AREA) zoomFactor=0;
+    //else if((getTileHeight()*getTileWidth()>80)) {}
+    double xPlayer = sprite.getX();
+    double yPlayer = sprite.getY();
+
+    int xPlayerTile = (int) xPlayer/tWidth;
+    int yPlayerTile = (int) yPlayer/tHeight;
+
+
+    // i dont think anything related to the tiles can be a double
+    //its fucking it up
+
+    int visXTile = Math.min((currentScreenWidth/tWidth), GRID_WIDTH);
+    int visYTile = Math.min((currentScreenHeight/tHeight),GRID_HEIGHT);
+
+    int xMin = Math.max ((xPlayerTile-(visXTile/2)-1),0);
+    int yMin = Math.max ((yPlayerTile-(visYTile/2)-1),0);
+    int xMax = Math.min ((xPlayerTile + (visXTile / 2) + 1), GRID_WIDTH);
+    int yMax = Math.min ((yPlayerTile + (visYTile / 2) + 1), GRID_HEIGHT);
+
+
+    double offX = xPlayer - (currentScreenWidth/2);
+    double offY = yPlayer - (currentScreenHeight/2);
+
+    int tileAdjustmentX = (int) offX%tWidth;
+    int tileAdjustmentY = (int) offY%tHeight;
+    int x = xMax-xMin;
+    int y = yMax-yMin;
+    System.out.print("visx " + x + " visy " +y+ "\n");
+    System.out.println("xPlayer: " + xPlayer);
+    System.out.println("yPlayer: " + yPlayer);
+
+
+    drawTile(xMin,yMin,xMax,yMax,tileAdjustmentX,tileAdjustmentY,g);
+
   }
 
-  private int getTileWidth()
+
+  private int getTileHeight()
   {
-    return currentScreenWidth / GRID_WIDTH + zoomFactor;
+    return 95;
+        //currentScreenHeight / GRID_HEIGHT + zoomFactor;
   }
+
+  private int getTileWidth() {return 168;}
+  // currentScreenWidth / GRID_WIDTH + zoomFactor;}
 
   private TileImage[][] translateTileImages(int[][] grid)
   {
@@ -92,16 +138,11 @@ public class HouseImage extends JPanel implements KeyListener
 
   private PlayerImage spawnSprite()
   {
-    for (int i = 0; i < GRID_WIDTH; i++)
-      for (int j = 0; j < GRID_HEIGHT; j++)
-        if(tileImages[i][j] instanceof FloorImage)
-        {
-           return new PlayerImage(i*getTileWidth(),j*getTileHeight(), 10);
-        }
-    return new PlayerImage(0,0,10);
+
+    return new PlayerImage(900,900,20);
   }
   /**
-   * needs more work to optimize loading speed
+   * needs more work to optimize loading speed/ get rid of randomization
    */
 //  private void decorateOutside(Graphics g)
 //  {
@@ -143,13 +184,9 @@ public class HouseImage extends JPanel implements KeyListener
   @Override
   public void paintComponent(Graphics g)
   {
-    drawTile(g);
-    //decorateOutside(g);
+    setVisibleFrame(g);
     drawSprite(g);
   }
-
-
-
 
   @Override
   public void keyTyped(KeyEvent e)
