7ba14654258eccaa0bcd970fce4b159f6ed19851
Thien-Cam Nguyen <tnguyen22@unm.edu>
2015-09-18 17:42:09 -0600

    Added dummy files for sprites and sound. Sound resource streams work both in IntelliJ and in JAR artifact.

--- src/Resources/SoundLoader.java
+++ src/Resources/SoundLoader.java
@@ -1,4 +1,3 @@
-
 /**
  * Created by Tess Daughton, September 13th 2015
  */
@@ -7,6 +6,7 @@ package Resources;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.BufferedInputStream;
 import java.io.File;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -25,7 +25,8 @@ public class SoundLoader
   {
     try
     {
-      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(("src/Resources/sound_resources/fantasy_sound/Wav/Footsteps/Footstep_Dirt_02.wav")).getAbsoluteFile());
+      //AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(("src/Resources/sound_resources/fantasy_sound/Wav/Footsteps/Footstep_Dirt_02.wav")).getAbsoluteFile());
+      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(this.getClass().getResourceAsStream("/Resources/sound_resources/Footstep_Dirt_02.wav")));
       Clip clip = AudioSystem.getClip();
       clip.open(audioInputStream);
       clip.start();
@@ -40,11 +41,11 @@ public class SoundLoader
   {
     try
     {
-      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(("src/Resources/sound_resources/fantasy_sound/Wav/Footsteps/Footstep_Dirt_03.wav")).getAbsoluteFile());
+      //AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(("src/Resources/sound_resources/fantasy_sound/Wav/Footsteps/Footstep_Dirt_03.wav")).getAbsoluteFile());
+      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(this.getClass().getResourceAsStream("/Resources/sound_resources/Footstep_Dirt_03.wav")));
       Clip clip = AudioSystem.getClip();
       clip.open(audioInputStream);
       clip.start();
-
     }
     catch(Exception ex)
     {
@@ -54,14 +55,13 @@ public class SoundLoader
 
   public void playBackgroundMusic()
   {
-
     try
     {
-      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(("src/Resources/sound_resources/Haunted_Woods/Haunted_Woods_Loop.wav")).getAbsoluteFile());
+      //AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(("src/Resources/sound_resources/Haunted_Woods/Haunted_Woods_Loop.wav")).getAbsoluteFile());
+      AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(this.getClass().getResourceAsStream("/Resources/sound_resources/Haunted_Woods_Loop.wav")));
       Clip clip = AudioSystem.getClip();
       clip.open(audioInputStream);
       clip.start();
-
     }
     catch(Exception ex)
     {
