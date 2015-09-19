4aad6c1cec387ee0f711becd6c020aefd191c5f2
mryu93 <chilunguni@unm.edu>
2015-09-18 18:12:45 -0600

    I added Alive interface. I thought it may help, but if you guys find it unnecessary, just go ahead and get rid of it. I also added PathFinder class that uses A* path finder algorithm. This is not completely implemented but I will try to implement for the weekend so that you guys can easily use this for zombies. And guys let keep commenting more so that we can communicate better when we edit codes. :)

--- src/model/Zombie.java
+++ src/model/Zombie.java
@@ -5,14 +5,27 @@
 package model;
 
 
-public class Zombie extends Movable
+public class Zombie extends Movable implements Alive
 {
   private static final double SPEED_WALK = 0.5f;
   private static final double RATE_ACT = 2.0f;
   private static final double DIST_SMELL = 7.0f;
+
+  // I thought maybe depending on the types of zombie, how much damage they take
+  // and how much health they recover each second may differ. If makes the game
+  // too difficult, then let's not do this. :D -Miri
+  private int originalHealth;
+  private int currentHeealth;
+  private double healingRate;
+  private double defenceRate;
+  private double damage;
+  private boolean dead;
+
+
   public Zombie(int x, int y, int radius, Tile location, Tile[][] grid)
   {
     super(x, y, radius, location, grid, GridOrientation.pickRandomOrientation());
+
   }
 
   private void setX(int x1)
@@ -23,5 +36,36 @@ public class Zombie extends Movable
   private void setY(int y1)
   {
     this.circle.setCenterY(y1);
+  }
+
+
+  //============================================================================
+  // Zombies should take damage from trap.
+  //============================================================================
+  @Override
+  public void getBurn(int damage)
+  {
+    this.damage = damage;
+  }
+
+  @Override
+  public int getHealth()
+  {
+    return currentHeealth;
+  }
+
+  @Override
+  public void updateCurrentStatus()
+  {
+    currentHeealth -= damage;
+    damage -= damage*defenceRate;
+
+    if(currentHeealth <= 0) dead = true;
+  }
+
+  @Override
+  public boolean isDead()
+  {
+    return dead;
   }
 }
